dca5971d7cadf72650093608a94e646c513e74d3
ravidomadiya1999@gmail.com <ravidomadiya1999@gmail.com>
2022-04-13 18:09:26 +0530

    Added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
 package core_java;
 
+/**
+ * This class of Helloworld example
+ * @author RAVI
+ */
 public class HelloWorld {
-	
-	public static void main(String [] args) {
-		System.out.println("Hello World");
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); // print the hello world
 	}
+	
 
 }
